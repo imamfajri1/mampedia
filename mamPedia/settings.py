be77581aaf11dacfa61b7faacd8825227ec8087b
Imam Fajri <imamfajri.ze@gmail.com>
2024-09-17 21:21:09 +0700

    Tugas 3 Done

--- mamPedia/settings.py
+++ mamPedia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--(3)cfyoa6&f@(q(&r$h3y@^bzgvw^&@)4uh2wt+^-jettag9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "imam-fajri-mampediax.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "imam-fajri-mampedia.pbp.cs.ui.ac.id"]
 
 
 # Application definition
