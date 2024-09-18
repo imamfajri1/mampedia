4c062c48cb27a4ab92ee71d756f88581ac3245bc
Imam Fajri <imamfajri.ze@gmail.com>
2024-09-18 13:40:25 +0700

    Fix settings

--- mamPedia/settings.py
+++ mamPedia/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","imam-fajri-mampedia.pbp.cs.ui.ac.id", "imam-fajri-mampedia.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/imam.fajri/mampedia", "https://pbp.cs.ui.ac.id/web/project/imam.fajri/mampedia"]
